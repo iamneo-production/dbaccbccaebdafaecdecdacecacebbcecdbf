8175a5748af5b9c95c6b3fb04c0e3bbbc612d17a
Iamneo <techadmin@iamneo.ai>
2023-09-14 07:10:42 +0000

    Backup Commit

--- format.ps1
+++ format.ps1
@@ -3,9 +3,13 @@ $month = Read-Host -Prompt "Enter the month (MM)"
 $year = Read-Host -Prompt "Enter the year (YYYY)" 
 
 if($date -match  '^\d{1,2}$' -and $month -match '^\d{1,2}$' -and $year -match '^d{4}$'){
-    $dateofBirth = Get-Date -Year $year -Month $month -Day $day
-    $formatteddate = $dateofBirth.ToString("dd-MM-yyyy")
-    Write-Host "Date of Birth: $formatteddate"
+    try{
+        $dateofBirth = [DateTime]::ParseExact("$year-$month-$day","yyyy-MM-dd",[System.Globalization.CultureInfo]::I)
+        $formatteddate = $dateofBirth.ToString("dd-MM-yyyy")
+        Write-Host "Date of Birth: $formatteddate"}
+        catch {
+            Write-Host "Invalid date"
+        }
 } else {
     Write-Host "Invalid input.Please input valid values for date,month and year"
 }
