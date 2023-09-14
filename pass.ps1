6c15e1dfb8847a97476f8f1f5e59aaa89681d3ca
Iamneo <techadmin@iamneo.ai>
2023-09-14 06:05:41 +0000

    Backup Commit

---
+++ pass.ps1
@@ -0,0 +1,13 @@
+$num1 = Read-Host "enter first number"
+$num2 = Read-Host "enter second number" 
+
+$num1 = [int]$num1 
+$num2 = [int]$num2 
+
+$tot = $num1 + $num2 
+
+if ($total -ge 70){
+    Write-Host "Pass"
+} else {
+    Write-Host "Fail"
+}
