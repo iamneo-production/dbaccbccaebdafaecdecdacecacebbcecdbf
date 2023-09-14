351638fb234097a66def01255e7041968d014ef1
Iamneo <techadmin@iamneo.ai>
2023-09-14 07:21:01 +0000

    Backup Commit

--- format.ps1
+++ format.ps1
@@ -1,4 +1,6 @@
 $date = Read-Host -Prompt "Enter the day (DD)"
 $month = Read-Host -Prompt "Enter the month (MM)" 
 $year = Read-Host -Prompt "Enter the year (YYYY)" 
-
+if ($day -match '^\d{1,2}$' -and $month -match '^\d{1,2}$' -and $year -match '^\d{4}$'){
+    $formattedDate = "{0:D2"
+}
