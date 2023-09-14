d877c0c8fdea7b7d2e7749c70e91c48919aafbf2
Iamneo <techadmin@iamneo.ai>
2023-09-14 07:25:42 +0000

    Backup Commit

--- format.ps1
+++ format.ps1
@@ -1,6 +1,9 @@
-$date = Read-Host -Prompt "Enter the day (DD)"
-$month = Read-Host -Prompt "Enter the month (MM)" 
-$year = Read-Host -Prompt "Enter the year (YYYY)" 
-if ($day -match '^\d{1,2}$' -and $month -match '^\d{1,2}$' -and $year -match '^\d{4}$'){
-    $formattedDate = "{0:D2"
+$date = Read-Host  "Enter the day (DD)"
+$month = Read-Host  "Enter the month (MM)" 
+$year = Read-Host  "Enter the year (YYYY)" 
+if ($date -match '^\d{1,2}$' -and $month -match '^\d{1,2}$' -and $year -match '^\d{4}$'){
+    $formattedDate = "{0:D2}-{1:D2}-{2}" -f [int]$date, [int]$month, $year 
+    Write-Host "Date of Birth: $formattedDate"
+} else{
+    Write-Host "Invalid"
 }
